d1ea8d2b0c929171a8dc3e8be179810667f65ac8
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2025-01-26 11:22:13 +0530

    created view for calendar added service for refresh browser on change in view

---
+++ mvcPractiseHTMLImpInterview/w3schoolsHowToImp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using w3schoolsHowToImp.Models;
+
+namespace w3schoolsHowToImp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mvcPractiseHTMLImpInterview/w3schoolsHowToImp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace w3schoolsHowToImp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
